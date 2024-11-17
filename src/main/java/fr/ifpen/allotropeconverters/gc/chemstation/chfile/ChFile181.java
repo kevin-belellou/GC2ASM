5742b5c63d4477107e5fb861cfa023a410e598c9
Kévin Belellou <1219617+kevin-belellou@users.noreply.github.com>
2024-11-17 21:34:27 +0100

    Fixes #44 - Retrieve unit from .ch file (#45)
    Read unit from .ch file and convert from it to picoampere when reading values

--- src/main/java/fr/ifpen/allotropeconverters/gc/chemstation/chfile/ChFile181.java
+++ src/main/java/fr/ifpen/allotropeconverters/gc/chemstation/chfile/ChFile181.java
@@ -1,20 +1,23 @@
 package fr.ifpen.allotropeconverters.gc.chemstation.chfile;
 
+import javax.measure.converter.UnitConverter;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 class ChFile181 extends ChFile {
+
     private static final int DATA_START = 6144; // https://github.com/chemplexity/chromatography/blob/master/Development/File%20Conversion/ImportAgilentFID.m
     private static final int START_TIME_POSITION = 282;
     private static final int END_TIME_POSITION = 286;
+    private static final int UNITS_POSITION = 4172;
     private static final int Y_OFFSET_POSITION = 4724;
     private static final int Y_SCALING_POSITION = 4732;
     private static final int DETECTOR_POSITION = 4213;
 
     ChFile181(RandomAccessFile input) throws IOException {
-        super(input, DATA_START, START_TIME_POSITION, END_TIME_POSITION, Y_OFFSET_POSITION, Y_SCALING_POSITION, DETECTOR_POSITION);
+        super(input, DATA_START, START_TIME_POSITION, END_TIME_POSITION, UNITS_POSITION, Y_OFFSET_POSITION, Y_SCALING_POSITION, DETECTOR_POSITION);
     }
 
     @Override
@@ -22,27 +25,27 @@ class ChFile181 extends ChFile {
         input.seek(DATA_START);
 
         values = new ArrayList<>();
-        long[] buffer = new long[]{0,0,0};
+        long[] buffer = new long[]{0, 0, 0};
+
+        UnitConverter unitConverter = unit.getConverterTo(PICO_AMPERE_UNIT);
 
         boolean endOfFile = false;
 
-        while(!endOfFile){
-            try{
-                buffer[2]= input.readShort();
+        while (!endOfFile) {
+            try {
+                buffer[2] = input.readShort();
 
-                if(buffer[2] != 32767){
-                    buffer[1]=buffer[2]+buffer[1];
-                    buffer[0]=buffer[1]+buffer[0];
+                if (buffer[2] != 32767) {
+                    buffer[1] = buffer[2] + buffer[1];
+                    buffer[0] = buffer[1] + buffer[0];
                 } else {
                     buffer[0] = (long) input.readShort() << 32;
                     buffer[0] = input.readInt() + buffer[0];
                     buffer[1] = 0;
                 }
 
-                values.add(buffer[0] * yScaling + yOffset);
-
-            }
-            catch (EOFException e){
+                values.add(unitConverter.convert(buffer[0] * yScaling + yOffset));
+            } catch (EOFException e) {
                 endOfFile = true;
             }
         }
